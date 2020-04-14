73791eafbf830198c38f08e24df93719c5480cef
Beng Tan <bengtan@users.noreply.github.com>
2020-04-14 14:55:24 +0800

    Add create/open/close/save/saveAs for projects.

--- src/core/Project.ts
+++ src/core/Project.ts
@@ -13,16 +13,49 @@ export class Project {
         this.isModified = false
     }
 
-    static async create(filename?: string): Promise<Project> {
-        const project = new Project(filename ? filename : '', new sqlite.Database(filename ? filename : ':memory:'))
+    // The following functions create new in-memory database,
+    // and load-to/save-from files
+    static async create(): Promise<Project> {
+        const project = new Project('', new sqlite.Database(':memory:'))
         await project.database.open()
         try {
             await initSchema(project.database)
+            project.isModified = true
         }
         catch (e) {
             project.database.close()
             throw e
         }
         return project
+    }
+
+    static async open(filename: string): Promise<Project> {        
+        const srcDb = new sqlite.Database(`file:${filename}?mode=ro`)
+        await srcDb.open()
+        try {
+            const newDb = await srcDb.backupTo(':memory:')
+            return new Project(filename, newDb)
+        }
+        finally {
+            srcDb.close()
+        }
+    }
+
+    close(): Promise<void> {
+        this.filename = ''
+        this.isModified = false
+        return this.database.close()
+    }
+
+    async save(): Promise<boolean> {
+        return this.filename ? this.saveAs(this.filename) : false
+    }
+
+    async saveAs(filename: string): Promise<boolean> {
+        await this.database.exec('VACUUM')
+        const destDb = await this.database.backupTo(filename)
+        this.isModified = false
+        destDb.close()
+        return true
     }
 }
