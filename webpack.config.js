25482a552567b38ab724af9e157aa54daaac50e6
Beng Tan <bengtan@users.noreply.github.com>
2020-04-23 09:02:01 +0800

    yarn add react-datepicker css-loader style-loader
    And configure webpack to import css files.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           }
         ]
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
