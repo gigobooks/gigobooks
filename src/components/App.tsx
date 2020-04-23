ce5e7bece4f784bb86d173b4c08fc0510d685a71
Beng Tan <bengtan@users.noreply.github.com>
2020-04-23 17:03:58 +0800

    Project.ts to be fully static. Add DebugScreen.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import Sidebar from 'react-sidebar'
-import { HashRouter, Link, Route, Switch, useParams } from "react-router-dom"
+import { HashRouter, Link, Route, Switch, useParams } from 'react-router-dom'
 import AccountOverview from './AccountOverview'
 import AccountDetail from './AccountDetail'
 import AccountNew from './AccountNew'
 import ContributeCapital from './ContributeCapital'
+import DebugScreen from './DebugScreen'
 
 const mql = window.matchMedia(`(min-width: 800px)`);
 
@@ -57,28 +58,32 @@ class App extends React.Component<{}, AppState> {
 
 const Menu = () => {
     return <div>
-        <Link to="/"><h2>Home</h2></Link>
-        <Link to="/account"><h2>Accounts</h2></Link>
-        <Link to="/account/new"><h2>New account</h2></Link>
-        <Link to="/contribute"><h2>Contribute</h2></Link>
+        <Link to='/'><h2>Home</h2></Link>
+        <Link to='/account'><h2>Accounts</h2></Link>
+        <Link to='/account/new'><h2>New account</h2></Link>
+        <Link to='/contribute'><h2>Contribute</h2></Link>
+        <Link to='/debug'><h2>Debug</h2></Link>
     </div>
 }
 
 const Main = () => {
     return <Switch>
-        <Route path="/contribute">
+        <Route path='/contribute'>
             <ContributeCapital />
         </Route>
-        <Route path="/account/new">
+        <Route path='/account/new'>
             <AccountNew />
         </Route>
-        <Route path="/account/:id">
-            <DispatchWithParams element="AccountDetail" />
+        <Route path='/account/:id'>
+            <DispatchWithParams element='AccountDetail' />
         </Route>
-        <Route path="/account">
+        <Route path='/account'>
             <AccountOverview />
         </Route>
-        <Route path="/">
+        <Route path='/Debug'>
+            <DebugScreen />
+        </Route>
+        <Route path='/'>
             <h1>Front page</h1>
         </Route>
     </Switch>
--- src/core/Project.ts
+++ src/core/Project.ts
@@ -4,69 +4,71 @@ import { Model } from 'objection'
 import prepopulate from './prepopulate'
 
 export class Project {
-    isModified: boolean
+    // isModified: boolean
+    static project: Project | undefined
+    static database: sqlite.Database
+    static knex: Knex
 
-    constructor(public filename: string, public database: sqlite.Database, public knex?: Knex) {
-        this.isModified = false
+    constructor(public filename: string, public database: sqlite.Database, public knex: Knex) {
     }
 
     // The following functions create new in-memory database,
     // and load-to/save-from files
-    static async create(): Promise<Project> {
-        const project = new Project('', new sqlite.Database(':memory:'))
-        await project.database.open()
+    static async create(): Promise<void> {
+        const db = new sqlite.Database(':memory:')
+        await db.open()
         try {
-            project.knex = makeKnex(':memory:', project.database)
-            await prepopulate(project.database, project.knex!)
+            const project = new Project('', db, makeKnex(':memory', db))
+            await prepopulate(project.database, project.knex)
             Project.bind(project)
-            Model.knex(project.knex)
-            project.isModified = true
         }
         catch (e) {
-            project.database.close()
+            db.close()
             throw e
         }
-        return project
     }
 
-    static async open(filename: string): Promise<Project> {        
+    static async open(filename: string): Promise<void> {        
         const srcDb = new sqlite.Database(`file:${filename}?mode=ro`)
         await srcDb.open()
         try {
             const newDb = await srcDb.backupTo(':memory:')
             const project = new Project(filename, newDb, makeKnex(filename, newDb))
             Project.bind(project)
-            Model.knex(project.knex)
-            return project
         }
         finally {
             srcDb.close()
         }
     }
 
-    close(): Promise<void> {
-        this.filename = ''
-        this.isModified = false
-        return this.database.close()
+    static async close(): Promise<void> {
+        const p = Project.project!
+        Project.project = undefined
+        p.filename = ''
+        return p.database.close()
     }
 
-    async save(): Promise<boolean> {
-        return this.filename ? this.saveAs(this.filename) : false
+    static async save(): Promise<boolean> {
+        if (Project.project && Project.project.filename) {
+            return Project.saveAs(Project.project.filename)
+        }
+        else {
+            return Promise.reject('No filename specified')
+        }
     }
 
-    async saveAs(filename: string): Promise<boolean> {
-        await this.database.exec('VACUUM')
-        const destDb = await this.database.backupTo(filename)
-        this.isModified = false
+    static async saveAs(filename: string): Promise<boolean> {
+        await Project.database.exec('VACUUM')
+        const destDb = await Project.database.backupTo(filename)
         destDb.close()
         return true
     }
 
-    // Store references to a database/knex so other code can use them globally
-    static database: sqlite.Database
-    static knex: Knex
+    // Store references so other code can access them globally
     static bind(p: Project): void {
+        Project.project = p
         Project.database = p.database
-        Project.knex = p.knex!
+        Project.knex = p.knex
+        Model.knex(p.knex)
     }
 }
