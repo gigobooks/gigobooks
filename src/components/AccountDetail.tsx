4020a4e3d06d2aa889b872105d93047b4daa91d1
Beng Tan <bengtan@users.noreply.github.com>
2020-04-24 15:46:51 +0800

    Flesh out src/components/AccountDetail.tsx.

--- src/components/AccountDetail.tsx
+++ src/components/AccountDetail.tsx
@@ -1,7 +1,38 @@
 import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import { Account, AccountTypeInfo } from '../core'
 
-export default function AccountDetail(props: {id?: string}) {
-    return <div>
-        <h1>Account Detail {props.id}</h1>
+export default function AccountDetail({id}: {id?: any}) {
+    const [account, setAccount] = React.useState<Account>()
+    React.useEffect(() => {
+        Account.query().findById(id).then((a: Account) => {
+            setAccount(a)
+        })
+    }, [id])
+
+    type FormData = { title: string }
+    const {register, errors, handleSubmit} = useForm<FormData>()
+    const onSubmit = async (data: FormData) => {
+        if (data.title != account!.title) {
+            account!.title = data.title
+            await account?.save()
+            setAccount(account)
+        }
+    }
+
+    return !!account && <div>
+        <h1>{account?.title}</h1>
+        <p>Type: {AccountTypeInfo[account.type!].label}</p>
+        {!account.isReserved && 
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor='title'>Title:</label>
+                <input name='title' ref={register({required: true})} defaultValue={account.title} />
+                {errors.title && 'Title is required'}
+            </div><div>
+                <input type='submit' value='Rename' />
+            </div>
+        </form>
+        }
     </div>
 }
