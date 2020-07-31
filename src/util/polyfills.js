5680916bb65a17012cf3f73f4d10552cbf98999c
Beng Tan <bengtan@users.noreply.github.com>
2020-07-31 13:05:16 +0800

    yarn add pdfjs-dist, worker-loader and polyfills.

--- src/util/polyfills.js
+++ src/util/polyfills.js
@@ -2,6 +2,14 @@
  * Copyright (c) 2020-present Beng Tan
  */
 
+const allsettled = require('promise.allsettled')
+allsettled.shim()
+
+// pdfjs-dist uses globalThis which might be undefined
+if (typeof window !== "undefined" && !("globalThis" in window)) {
+    window.globalThis = window
+}
+
 Object.defineProperty(String.prototype, 'isEnum', {
     value(_enum) {
         for (let k in _enum) {
