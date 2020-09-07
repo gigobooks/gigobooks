gigobooks/gigobooks

commit 15c901c663c18bf7ae2b71fc9175f3ecceba7a11

Fix an infinite loop.

src/components/ContributeCapital.tsx
@@ -75,7 +75,7 @@ export default function ContributeCapital(props: Props) {
                 elements: [{currency}, {currency}],
             })
         }
-    }, [props.arg1, transaction && transaction.id ? transaction.updatedAt : 0])
+    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/Purchase.tsx
@@ -110,7 +110,7 @@ export default function Purchase(props: Props) {
             setTransaction(Transaction.construct({}))
             clearForm()
         }
-    }, [props.arg1, transaction && transaction.id ? transaction.updatedAt : 0])
+    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/Sale.tsx
@@ -105,7 +105,7 @@ export default function Sale(props: Props) {
             setTransaction(Transaction.construct({}))
             clearForm()
         }
-    }, [props.arg1, transaction && transaction.id ? transaction.updatedAt : 0])
+    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/TransactionDetail.tsx
@@ -88,7 +88,7 @@ export default function TransactionDetail(props: Props) {
                 elements: [{currency}, {currency}],
             })
         }
-    }, [props.arg1, transaction && transaction.id ? transaction.updatedAt : 0])
+    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/BillPayment.tsx
@@ -50,7 +50,7 @@ export default function BillPayment(props: Props) {
         })
 
         return () => {mounted=false}
-    }, [transaction.updatedAt])
+    }, [transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/InvoicePayment.tsx
@@ -50,7 +50,7 @@ export default function InvoicePayment(props: Props) {
         })
 
         return () => {mounted=false}
-    }, [transaction.updatedAt])
+    }, [transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

commit 6883a6b8890a9f875cbf05572294e4e76692d18d

Fix up 'Save and new' button on an existing sale or purchase.
This is a regression introduced in 15c901c6 (which itself is a
bugfix for 4a9b7e66).

src/components/ContributeCapital.tsx
@@ -75,7 +75,7 @@ export default function ContributeCapital(props: Props) {
                 elements: [{currency}, {currency}],
             })
         }
-    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
+    }, [props.arg1, transaction && transaction.id && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/Purchase.tsx
@@ -111,7 +111,7 @@ export default function Purchase(props: Props) {
             setTransaction(Transaction.construct({}))
             clearForm()
         }
-    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
+    }, [props.arg1, transaction && transaction.id && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/Sale.tsx
@@ -106,7 +106,7 @@ export default function Sale(props: Props) {
             setTransaction(Transaction.construct({}))
             clearForm()
         }
-    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
+    }, [props.arg1, transaction && transaction.id && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {

src/components/TransactionDetail.tsx
@@ -88,7 +88,7 @@ export default function TransactionDetail(props: Props) {
                 elements: [{currency}, {currency}],
             })
         }
-    }, [props.arg1, transaction && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
+    }, [props.arg1, transaction && transaction.id && transaction.updatedAt ? transaction.updatedAt.toString() : 0])
 
     const onSubmit = (data: FormData) => {
         if (!validateFormData(form, data)) {