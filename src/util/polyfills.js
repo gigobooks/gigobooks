04d663c7b17fb0254d7f0a6ce0d271e3f5b27808
Beng Tan <bengtan@users.noreply.github.com>
2020-12-31 15:23:11 +0800

    Prospective fix: 'arrayBuffer is not a function'
    Safari/iOS

--- src/util/polyfills.js
+++ src/util/polyfills.js
@@ -17,3 +17,20 @@ Object.defineProperty(String.prototype, 'isEnum', {
         return false
     }
 })
+
+// https://gist.github.com/hanayashiki/8dac237671343e7f0b15de617b0051bd
+if ('File' in  self) {
+    File.prototype.arrayBuffer = File.prototype.arrayBuffer || myArrayBuffer;
+}
+Blob.prototype.arrayBuffer = Blob.prototype.arrayBuffer || myArrayBuffer;
+
+function myArrayBuffer() {
+    // this: File or Blob
+    return new Promise((resolve) => {
+        let fr = new FileReader();
+        fr.onload = () => {
+            resolve(fr.result);
+        };
+        fr.readAsArrayBuffer(this);
+    })
+}
